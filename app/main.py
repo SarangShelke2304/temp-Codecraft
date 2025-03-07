73af29f3a597b67e580ee1990c25dc9b1f607fe5
pravinpk9 <83226357+pravinpk9@users.noreply.github.com>
2025-03-07 23:43:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from app.api.endpoints import workflows
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
 origins = [
     "http://localhost",
-    "http://localhost:5173",
-    # Add any other domains as needed
+    "http://localhost:5173"
 ]
 
 app.add_middleware(
@@ -15,8 +16,13 @@ app.add_middleware(
     allow_methods=["*"],              # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],              # Allow all headers
 )
+
 app.include_router(workflows.router, prefix="/workflows")
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to CodeCraft API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
